23d192c3af73f028536cd9f134abee748fda7490
pavankumar0715 <pavankumarg2002@gmail.com>
2024-03-06 17:12:34 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   Assignment1/.gitignore 	new file:   Assignment1/.idea/.gitignore 	new file:   Assignment1/.idea/encodings.xml 	new file:   Assignment1/.idea/misc.xml 	new file:   Assignment1/.idea/vcs.xml 	new file:   Assignment1/pom.xml 	new file:   Assignment1/src/main/java/com/targetindia/programs/InWords.java 	new file:   Assignment1/src/main/java/com/targetindia/programs/PrintCalendar.java 	new file:   Assignment1/src/main/java/com/targetindia/utils/KeyboardUtil.java

---
+++ Assignment1/src/main/java/com/targetindia/programs/InWords.java
@@ -0,0 +1,63 @@
+package com.targetindia.programs;
+
+import com.targetindia.utils.KeyboardUtil;
+
+public class InWords {
+
+    private static final String[] units = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private static final String[] teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private static final String[] thousands = {"", "Thousand", "Million", "Billion"};
+    private static final String[] ind = {"", "Lakh", "Crore"};
+    private static String helper(int n) {
+        if (n == 0) {
+            return "";
+        }
+        else if (n < 10) {
+            return units[n] + " ";
+        }
+        else if (n < 20) {
+            return teens[n - 10] + " ";
+        }
+        else if (n<100){
+            return tens[n / 10] + " " + units[n % 10] + " ";
+        }
+        else {
+            return units[n/100]+" hundred "+helper(n%100);
+        }
+    }
+    public static String inWords(int n){
+        if (n == 0) {
+            return "Zero";
+        }
+
+        int i = 0;
+        String words = "";
+
+        while (n != 0) {
+            if (n % 1000 != 0) {
+                words = helper((int)(n % 1000)) + thousands[i] + " " + words;
+            }
+            n /= 1000;
+            i++;
+        }
+
+        return words;
+    }
+
+
+
+    public static void main(String[] args) {
+        int num=0;
+        num=KeyboardUtil.getInt("Enter a number:");
+        System.out.println("45646 in words is "+inWords(45646));
+        System.out.println("12 in words is "+inWords(12));
+        System.out.println("8468782 in words is "+inWords(8468782));
+        System.out.println("99922 in words is "+inWords(99922));
+        System.out.println(num + " in words is " +inWords(num));
+
+
+
+
+    }
+}
---
+++ Assignment1/src/main/java/com/targetindia/programs/PrintCalendar.java
@@ -0,0 +1,79 @@
+package com.targetindia.programs;
+
+import com.targetindia.utils.KeyboardUtil;
+public class PrintCalendar {
+
+    private static final String[] DAY_NAMES = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+    static boolean isValid(int month, int year){
+        return ((month<1 || month>12) && year<0);
+    }
+    static int getDaysInMonth(int month, int year) {
+        int maxDays = 31;
+
+        switch (month) {
+            case 2:
+                maxDays = isLeap(year) ? 29 : 28;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                maxDays = 30;
+        }
+        return maxDays;
+    }
+
+    static boolean isLeap(int year){
+        return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
+    }
+
+
+    private static int calculateFirstDayOfMonth(int month, int year) {
+        int day = 1;
+        int m = (month < 3) ? (month + 10) : (month - 2);
+        int y = (month < 3) ? (year - 1) : year;
+        int c = y / 100;
+        y %= 100;
+
+        return (day + (13 * (m + 1) / 5) + y + (y / 4) + (c / 4) + (5 * c)) % 7 - 1;
+    }
+
+    public static void printCalendar(int month, int year){
+        for (String dayName : DAY_NAMES) {
+            System.out.print(" " + dayName);
+        }
+        System.out.println();
+
+
+        int spaces =  calculateFirstDayOfMonth(month, year);
+        if (spaces < 0)
+            spaces = 6;
+
+        for (int i = 0; i < spaces; i++) {
+            System.out.print("    ");
+        }
+
+
+
+        int daysInMonth = getDaysInMonth(month, year);
+        for (int day = 1; day <= daysInMonth; day++) {
+            System.out.printf(" %4d ", day);
+
+            if (((day + spaces) % 7 == 0) || (day == daysInMonth))
+                System.out.println();
+        }
+        return;
+    }
+
+
+    public static void main(String [] args){
+        int month, year;
+        month=KeyboardUtil.getInt("Enter the month:");
+        year=KeyboardUtil.getInt("Enter the year:");
+
+        printCalendar(month, year);
+
+
+    }
+}
